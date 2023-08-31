2dd05a6838d237fdeaed90031bfeb9fddce2af63
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-08-31 20:36:42 +0100

    finished quizgame

--- project1.js
+++ project1.js
@@ -2,33 +2,44 @@ const prompt = require("prompt-sync")();
 
 console.log("Welcome to the quiz!")
 
-const answer1 = prompt("What is the brain of the computer?: ")
-const correct_answer1 = "CPU" 
+let correctAnswers = 0; 
+let totalQuestions = 3; 
 
-if (answer1.toUpperCase() === correct_answer1) { 
+
+const answer1 = prompt("What is the brain of the computer?: ")
+const correctAnswer1 = "CPU" 
+
+if (answer1.toUpperCase() === correctAnswer1) { 
     console.log("That is correct!");
+    correctAnswers++; 
 } else { 
     console.log("That is wrong..."); 
 }
 
  
 const answer2 = prompt("What is better, Mercedes or BMW?: ")
-const correct_answer2 = "BMW" 
+const correctAnswer2 = "BMW" 
 
-if (answer2.toUpperCase() === correct_answer2) { 
+if (answer2.toUpperCase() === correctAnswer2) { 
     console.log("That is correct!");
+    correctAnswers++
 } else { 
     console.log("That is wrong..."); 
 }
 
  
 const answer3 = prompt("What is the capital of Turkiye?: ")
-const correct_answer3 = "ANKARA" 
+const correctAnswer3 = "ANKARA" 
 
-if (answer3.toUpperCase() === correct_answer3) { 
+if (answer3.toUpperCase() === correctAnswer3) { 
     console.log("That is correct!");
+    correctAnswers++ 
 } else { 
     console.log("That is wrong..."); 
 }
 
+const percent = Math.round((correctAnswers / totalQuestions) * 100)
+
+console.log("You got", correctAnswers, "questions correct!"); 
+console.log("You got", percent, "%"); 
  
