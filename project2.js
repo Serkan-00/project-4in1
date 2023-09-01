4899b8d5009eeaa6acddec2657a70138d053aaaa
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-01 12:15:31 +0100

    guess the number done

--- project2.js
+++ project2.js
@@ -1,11 +1,22 @@
 const prompt = require("prompt-sync")(); 
 
-
 const target = Math.round(Math.random() * 100) // this gives a random number between 0-100
 
+let guesses = 0 
 console.log(target)
 
-let guesses = 0 
+while (true) { 
+ guesses++; 
 
-const guess = Number(prompt("Guess the number (0-100): ")); // the Number converts to number
+ const guess = Number(prompt("Guess the number (0-100): ")); // the Number converts to number
+ if (guess > target) { 
+      console.log("Your guess is too high.")
+ } else if (guess < target) {
+    console.log("Your guess is too low.")
+} else { 
+     console.log("You guessed it!")
+  break; 
+}
+}
 
+console.log("You guessed it in", guesses, "tries!"); 
