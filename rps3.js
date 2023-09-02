f28e067ba88a5ca1ff8bd2a7b3a0e7b86777937b
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-02 14:41:27 +0100

    random index

--- rps3.js
+++ rps3.js
@@ -8,3 +8,7 @@ const choice = prompt("Enter Rock, Paper or Scissors: ")
 if (choice !== "Rock" && choice !== "Paper" && choice !== "Scissors") { 
     console.log("Please enter a valid choice.")
 }
+
+const choices = ["Rock", "Paper", "Scissors"]
+const randomIndex = Math.round(Math.random() * 2) 
+const computerChoice = choices[randomIndex]
