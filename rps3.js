8693d0f61594ce46cc63931c8c596e57f1f59205
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-04 14:05:32 +0100

    rps continue loop

--- rps3.js
+++ rps3.js
@@ -4,11 +4,31 @@ let wins = 0
 let losses = 0 
 let ties = 0 
 
-const choice = prompt("Enter Rock, Paper or Scissors: ")
-if (choice !== "Rock" && choice !== "Paper" && choice !== "Scissors") { 
+while (true) { 
+const playerChoice = prompt("Enter Rock, Paper or Scissors: ")
+if (playerChoice !== "Rock" && playerChoice !== "Paper" && playerChoice !== "Scissors") { 
     console.log("Please enter a valid choice.")
+    continue; 
 }
 
 const choices = ["Rock", "Paper", "Scissors"]
 const randomIndex = Math.round(Math.random() * 2) 
-const computerChoice = choices[randomIndex]
+const computerChoice = choices[randomIndex]; 
+
+console.log("The computer chose:", computerChoice); 
+
+if (computerChoice === playerChoice) { 
+    console.log("Draw!"); 
+    ties ++;
+} else if ( 
+    (playerChoice === "Paper" && computerChoice === "Rock") || 
+    (playerChoice === "Rock" && computerChoice === "Scissors") || 
+    (playerChoice === "Scissors" && computerChoice === "Paper")
+) { 
+    console.log("Wins!")
+    wins++
+} else { 
+    console.log("Lost!")
+    losses ++; 
+}
+}
